9490bbf01a24536690266ee47429ff3301cd82d8
Ricardo Trindade <costatrindade01@gmail.com>
2022-10-25 18:09:03 +0100

    Created ObjectEmployeeArrayCreator Class

--- Employee_CSV_Migrator/src/main/java/org/sparta/sonic/ReadCSV.java
+++ Employee_CSV_Migrator/src/main/java/org/sparta/sonic/ReadCSV.java
@@ -10,17 +10,24 @@ public class ReadCSV {
 
     }
 
-    public static void connectToFile(String filename){
+    public static String[] connectToFile(String filename){
+
+        String[] strArray = new String[10002];
+
         String line = "";
         try (FileReader fileReader = new FileReader(filename)){
             BufferedReader bufferedReader = new BufferedReader(fileReader);
+            int i=0;
+            bufferedReader.readLine();
             while((line = bufferedReader.readLine()) != null){
                 System.out.println(line);
+                strArray[i++] = line;
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return strArray;
     }
 }
--- Employee_CSV_Migrator/src/main/java/org/sparta/sonic/Starter.java
+++ Employee_CSV_Migrator/src/main/java/org/sparta/sonic/Starter.java
@@ -3,6 +3,10 @@ package org.sparta.sonic;
 public class Starter {
 
     public static void start() {
-        ReadCSV.connectToFile("src/main/resources/EmployeeRecordsMini.csv");
+        String[] x = ReadCSV.connectToFile("src/main/resources/EmployeeRecordsMini.csv");
+
+        Employee[] array = ObjectEmployeeArrayCreator.objectEmployeeArrayCreator(x);
+
+        //System.out.println(array[1].getEmail());
     }
 }
